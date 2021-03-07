579bccf9cf26d952b4c6d972ff5917128aeeaa2f
Petr Portnov <gh@progrm-jarvis.ru>
2021-03-08 00:44:15 +0300

    feat: implement command line tool
    This supports configuring:
    - hostname (required)
    - port (defaults to 25565)
    - timeout (defaults to 5 seconds)
    - defaults to (StatusCode)

--- src/main.rs
+++ src/main.rs
@@ -12,6 +12,51 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-fn main() {
-    println!("Hello, world!");
+use std::process::exit;
+use std::str::FromStr;
+
+use clap::Clap;
+use craftping::{Error, Response};
+use craftping::tokio::ping;
+use tokio::time::error::Elapsed;
+use tokio::time::timeout;
+
+use crate::duration::TimeoutDuration;
+use crate::output_flavor::OutputFlavor;
+
+mod duration;
+mod output_flavor;
+
+/// Options of the command line utility.
+#[derive(Clap)]
+struct CommandLineOptions {
+    /// Hostname of the server
+    hostname: String,
+
+    /// Port of the server
+    #[clap(short, long, default_value = "25565")]
+    port: u16,
+
+    /// Timeout after which the request should be considered failed
+    #[clap(short, long, default_value = "5s")]
+    timeout: TimeoutDuration,
+
+    /// Flavor of the output
+    #[clap(short, long, default_value = "status_code")]
+    flavor: OutputFlavor,
+}
+
+#[tokio::main(flavor = "current_thread")]
+async fn main() {
+    let options: CommandLineOptions = CommandLineOptions::parse();
+    match timeout(
+        *options.timeout,
+        ping(options.hostname.as_str(), options.port),
+    )
+        .await
+    {
+        Ok(Ok(response)) => options.flavor.handle_response(response),
+        Ok(Err(error)) => options.flavor.handle_error(error),
+        Err(timeout) => options.flavor.handle_timeout(timeout),
+    };
 }
